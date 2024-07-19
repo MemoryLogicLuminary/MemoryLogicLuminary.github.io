e954f685bff67247c2a2274f4900065b5cb4bf18
MemoryLogicLuminary <memorylogicdev@gmail.com>
2024-07-19 14:05:35 +0400

    something2

--- model-output.js
+++ model-output.js
@@ -7,7 +7,7 @@ var createScene = function () {
     camera.attachControl(canvas, true);
 
     var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-    var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+    // var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
     var textureURLs = [
         "https://example.come/texture1", // blank texture (on purpose)
@@ -21,6 +21,18 @@ var createScene = function () {
     
     var cube = BABYLON.MeshBuilder.CreateBox("cube", { size: 4 }, scene);
     cube.position = new BABYLON.Vector3(0, 1, 0); 
+
+    var cube2 = BABYLON.MeshBuilder.CreateBox("cube", { size: 4 }, scene);
+    cube2.position = new BABYLON.Vector3(0, 1, -20);
+
+    var texture2 = new BABYLON.Texture("https://i1.sndcdn.com/artworks-z1uZrU59JAqSMbRY-2MOcwQ-t1080x1080.jpg", scene);
+
+    var material2 = new BABYLON.StandardMaterial("material2", scene);
+    material2.diffuseTexture = texture2;
+    cube2.material = material2;
+
+    cube2.rotation.y = Math.PI;
+    cube2.rotation.x= Math.PI/9;
 
     
     function applyRandomTexture() {
